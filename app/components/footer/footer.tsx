0b700a53e20df106cdc5ee641ee5439255a4ed83
DevOps42 <leoneider42@gmail.com>
2023-05-06 23:37:39 -0500

    wip: :construction: added footer sections

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,4 +1,41 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-teal-100">
+      <div className="container mx-auto px-4 py-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <h1>Contáctenos</h1>
+            <p>Línea atención (607) 569 44 44</p>
+            <p>Carrera 12 Nº 10-00 Centro Ocaña, Norte de Santander</p>
+            <p>notificaciones@crediservir.com</p>
+          </div>
+          <div>
+            <h1>Corporativo</h1>
+            <p>Quiénes somos</p>
+            <p>Informes de Gestión</p>
+            <p>Gestión de Riesgos</p>
+            <p>Gestión Ambiental</p>
+            <p>Pensamiento Cooperativo</p>
+          </div>
+          <div>
+            <h1>Enlaces de Interés</h1>
+            <p>Tasas Efectivas Anuales</p>
+            <p> Tarifas Productos y Servicios</p>
+            <p> Sistema Gestión de Calidad</p>
+            <p> Reconocimientos</p>
+            <p> Reconocimientos Fundación Crediservir</p>
+            <p> Documentos Institucionales</p>
+          </div>
+          <div>
+            <h1>Servicio al Asociado</h1>
+            <p> Información legal</p>
+            <p>Protección de Datos</p>
+            <p>S.A.C.</p>
+            <p>Actualización de datos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Footer;
