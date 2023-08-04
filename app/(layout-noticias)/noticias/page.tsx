ce1db1ffe4952305dc33d2ebc1458bfe04eee99f
trig804142_Siigo <leoneider.trigos@siigo.com>
2023-08-03 21:56:38 -0500

    wip: :construction: add page news

--- app/(layout-noticias)/noticias/page.tsx
+++ app/(layout-noticias)/noticias/page.tsx
@@ -1,13 +1,30 @@
+"use client";
 import { Navbar } from "@/app/components";
+import { noticias } from "@/app/services/noticias";
+import { Card } from "flowbite-react";
 
 function Home() {
   return (
     <>
-      <Navbar></Navbar>
-      <div className="container mx-auto px-4">
+      <Navbar />
+      <div className="container mx-auto px-4 pt-48">
         <div>Bienvenidos a la seccion de noticias</div>
+
+        {noticias.map((noticia, idx) => (
+          <div key={idx} className="max-w-sm">
+            <Card imgAlt={noticia.title} imgSrc={noticia.image}>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {noticia.title}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {noticia.description}
+              </p>
+            </Card>
+          </div>
+        ))}
       </div>
     </>
   );
 }
+
 export default Home;
--- app/components/noticias/Noticias.tsx
+++ app/components/noticias/Noticias.tsx
@@ -8,40 +8,7 @@ import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOu
 
 import { Carousel, CustomFlowbiteTheme } from "flowbite-react";
 import ButtonNoticias from "@/app/components/button/Button";
-
-export interface NoticiasProps {
-  title: string;
-  description: string;
-  image: string;
-  date: string;
-  href: string;
-}
-
-const noticias: NoticiasProps[] = [
-  {
-    title: "Ganadores Concurso Crediservir Lovers",
-    description: "Conozca los ganadores del Concurso Crediservir Lovers",
-    image: "/noticias/img_0200.jpg",
-    date: "21 Ene, 2023",
-    href: "/",
-  },
-  {
-    title: "Curso virtual de cooperativismo",
-    description:
-      "Crediservir publica fechas del curso de cooperativismo en julio",
-    image: "/noticias/noticia-02.jpg",
-    date: "30 Mar, 2023",
-    href: "/",
-  },
-  {
-    title: "Inauguración Copa Credisito en Ocaña.",
-    description:
-      "El domingo 23 de abril se inauguró la primera temporada del campeonato Copa Credisito 2023.",
-    image: "/noticias/noticia-03.jpg",
-    date: "03 Jul, 2023",
-    href: "/",
-  },
-];
+import { noticias } from "@/app/services/noticias";
 
 const customTheme: CustomFlowbiteTheme["carousel"] = {
   root: {
