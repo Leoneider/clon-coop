09dda4fa054cb4967d066fb4f111956b98d9fad4
trig804142_Siigo <leoneider.trigos@siigo.com>
2023-08-10 17:28:42 -0500

    wip: :construction: added new colum

--- app/(noticias)/noticias/[id]/page.tsx
+++ app/(noticias)/noticias/[id]/page.tsx
@@ -14,7 +14,17 @@ function Noticia({ params }: { params: { id: string } }) {
 
   const noticia = findNoticia(id);
   return (
-    <div className="container mx-auto px-4 pt-48 pb-14">{noticia?.title}</div>
+    <div className="container mx-auto px-4 pt-48 pb-14">
+      <div className="grid grid-cols-7 justify-center">
+        <div className="col-span-6">
+          <p className="text-start">{noticia?.description}</p>
+        </div>
+        <div className="w-56 bg-amber-800">
+          <p>hola</p>
+        </div>
+      </div>
+      <p className="text-2xl text-primary">{noticia?.title}</p>
+    </div>
   );
 }
 
--- app/(noticias)/noticias/services/noticias.service.ts
+++ app/(noticias)/noticias/services/noticias.service.ts
@@ -27,7 +27,21 @@ export const noticias: INoticias[] = [
   {
     id: "4",
     title: "Ganadores Concurso Crediservir Lovers",
-    description: "Conozca los ganadores del Concurso Crediservir Lovers",
+    description: `Crediservir realiza el proceso de acreditación del Sello 100% Cooperativa de manera voluntaria, 
+    con el propósito de evaluar la gestión que desarrolla la entidad frente al desempeño de los principios
+    cooperativos. Obtener este Sello es una muestra del cumplimiento de los parámetros establecidos para 
+    considerarse como una entidad que ejerce a cabalidad el objeto social, aplicando cada uno de los
+    principios cooperativos y así ratificar el compromiso de generar valor a nuestros asociados,
+    bajo un modelo socio empresarial que antes de buscar el lucro, propende por el bienestar y 
+    el desarrollo de la base social.
+
+    DEFINICIÓN DEL SELLO 100% COOPERATIVA
+
+    El Sello 100% Cooperativa es un distintivo que otorga CONFECOOP 
+    a las entidades que desarrollan sus actividades de acuerdo con la 
+    naturaleza y los principios cooperativos, reconociendo el carácter
+    empresarial, la evaluación del cumplimiento de estándares sociales y 
+    financieros, para incentivar al mismo tiempo la cultura del mejoramiento continuo.`,
     image: "/noticias/img_0200.jpg",
     date: "21 Ene, 2023",
   },
